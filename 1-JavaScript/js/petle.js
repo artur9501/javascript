1f892f33ccb9dea3c46d2f9990c374ce3890e62a
Artur Młodziński <artur.mlody11@wp.pl>
2018-04-22 15:53:12 +0200

    pętle2

---
+++ 1-JavaScript/js/petle.js
@@ -0,0 +1,46 @@
+//for (var i = 1; i <= 5; i++) {
+//    console.log(i);
+//}
+
+
+//var tablica = ["Krystian", "Monika", "Danuta"];
+
+//for (var i = 0; i < 3; ++i) {
+//    console.log("element z indexem: " + i + " ma wartosc " + tablica[i]);
+//}
+
+//tablica.forEach(function (element, index) {
+//    console.log("element z indexem: " + index + " ma wartość " + element);
+//});
+
+//var it = 0;
+//while (it < 10) {
+//    console.log(it);
+//    it++;
+//}
+
+//var iter = 11;
+//do {
+//    console.log(iter);
+//    iter++;
+//} while (iter < 10)
+
+//var a = 0;
+//
+//while (a < 10) {
+//    console.log(a++);
+//
+//    if (a == 5) {
+//        break;
+//    }
+//}
+
+for (var b = 0; b < 10; ++b) {
+    if (b == 5) {
+        continue;
+    } else {
+        console.log(b);
+
+    }
+    console.log("-----");
+}
