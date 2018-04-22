f5d3ba6ec87cf26887978c2d25cdc5e6163f9a3e
Artur Młodziński <artur.mlody11@wp.pl>
2018-04-22 11:26:34 +0200

    funkcje

---
+++ 1-JavaScript/js/funkcje.js
@@ -0,0 +1,30 @@
+function dodajLiczby(pierwszaLiczba, drugaLiczba) {
+    var wynik = pierwszaLiczba + drugaLiczba;
+    //    console.log(wynik);
+    return wynik;
+}
+
+dodajLiczby(4, 7);
+
+var wynikZFunkcji = dodajLiczby(9, 7);
+
+console.log(wynikZFunkcji);
+
+
+//function aktualnyRok() {
+//    console.log("2018");
+//}
+//
+//var aktualnyMiesiac = function () {
+//    console.log("kwiecien");
+//}
+//
+//console.log(aktualnyMiesiac);
+//
+//aktualnyMiesiac();
+//aktualnyMiesiac();
+//aktualnyMiesiac();
+//
+//aktualnyRok();
+//aktualnyRok();
+//aktualnyRok();
