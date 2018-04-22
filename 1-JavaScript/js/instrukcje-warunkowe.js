d9ab6ccace76647f24bdfce17ab45c395a8b6149
Artur Młodziński <artur.mlody11@wp.pl>
2018-04-22 14:49:49 +0200

    pętle

---
+++ 1-JavaScript/js/instrukcje-warunkowe.js
@@ -0,0 +1,25 @@
+//var wzrostMateusza = 140;
+//var wzrostOlgi = 160;
+//
+//if (wzrostOlgi < wzrostMateusza) {
+//    console.log("Mateusz jest wyższy od Olgi");
+//} else if (wzrostOlgi == wzrostMateusza) {
+//    console.log("Są równi");
+//} else {
+//    console.log("Mateusz jest niższy od Olgi");
+//}
+
+var kolor = 'zielony';
+switch (kolor) {
+    case 'czerwony':
+        console.log('Kolor czerwony')
+        break;
+    case 'zielony':
+        console.log('Kolor zielony')
+        break;
+    case 'niebieski':
+        console.log('Kolor niebieski')
+        break;
+    default:
+        console.log('Inny kolor')
+}
